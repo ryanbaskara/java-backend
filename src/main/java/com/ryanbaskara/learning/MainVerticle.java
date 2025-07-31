6a61ce858ece10c1ca818226eb899f89010846f7
Ryan Baskara <ryan.baskara@bukalapak.com>
2025-08-01 06:49:26 +0700

    add docker

--- src/main/java/com/ryanbaskara/learning/MainVerticle.java
+++ src/main/java/com/ryanbaskara/learning/MainVerticle.java
@@ -9,6 +9,7 @@ import com.ryanbaskara.learning.presentation.handler.InstrumentHandler;
 import com.ryanbaskara.learning.presentation.handler.UserHandler;
 import com.ryanbaskara.learning.presentation.route.InstrumentRoute;
 import com.ryanbaskara.learning.presentation.route.UserRoute;
+import io.github.cdimascio.dotenv.Dotenv;
 import io.vertx.rxjava3.core.AbstractVerticle;
 import io.vertx.rxjava3.ext.web.Router;
 import io.vertx.rxjava3.mysqlclient.MySQLPool;
@@ -28,9 +29,11 @@ public class MainVerticle extends AbstractVerticle {
         UserHandler userHandler = new UserHandler(getUsersUseCase);
         UserRoute.configure(router, userHandler);
 
+        Dotenv env = Dotenv.load();
+        int serverPort = Integer.parseInt(env.get("SERVER_PORT"));
         vertx.createHttpServer()
                 .requestHandler(router)
-                .listen(8888)
-                .subscribe(http -> System.out.println("Server started"));
+                .listen(serverPort)
+                .subscribe(http -> System.out.printf("Server started at localhost:%d%n", serverPort));
     }
 }
